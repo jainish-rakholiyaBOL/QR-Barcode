6d0ab74352322547b29c33067535f015f7a9df40
“jainish-rakholiyaBOL” <jainish.rakholiya@lji.io>
2024-01-30 15:35:12 +0530

    DID SOME RANDOM CHANGES

--- app/src/main/java/com/devedroy/qrbarcode/MainActivity.kt
+++ app/src/main/java/com/devedroy/qrbarcode/MainActivity.kt
@@ -5,63 +5,43 @@ import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.devedroy.qrbarcode.databinding.ActivityMainBinding
 import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.core.app.ActivityCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class QRScanner(private val ctx: Context, val attributeSet: AttributeSet) : LinearLayout(ctx, attributeSet) {
     private val CAMERA_PERMISSION_CODE = 123
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+    private val binding = ActivityMainBinding.inflate(LayoutInflater.from(context), this, true)
+    init {
         binding.btnScan.setOnClickListener {
             triggerScan()
         }
-
-        binding.btnTest.setOnClickListener {
-            startActivity(Intent(this@MainActivity, TestActivity::class.java))
-        }
     }
+
 
     private fun triggerScan() {
         if (ContextCompat.checkSelfPermission(
-                this, Manifest.permission.CAMERA
+                context, Manifest.permission.CAMERA
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             openScanner()
         } else {
             ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE
+                context as Activity, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE
             )
         }
     }
 
     private fun openScanner() {
-        startActivity(Intent(this, BarCodeScannerActivity::class.java))
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults.isNotEmpty() && requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openScanner()
-            } else if (!ActivityCompat.shouldShowRequestPermissionRationale(
-                    this, Manifest.permission.CAMERA
-                )
-            ) {
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivityForResult(intent, CAMERA_PERMISSION_CODE)
-            }
-        }
+        context.startActivity(Intent(context, BarCodeScannerActivity::class.java))
     }
 
 }
